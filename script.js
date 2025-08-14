d1974d501f70462517a6d9c4261e7164bf0f9750
RobertoColatto <roberto.colatto@unesp.br>
2025-08-13 21:56:09 -0300

    feat: update index.html, style.css and script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 window.addEventListener("load", () => {
     const expressao = document.getElementById("expressao");
     const texto = document.getElementById("texto");
+    const status = document.getElementById("status");
 
     function validar() {
-        try {
-            const regex = new RegExp(expressao.value);
-            if(regex.test(texto.value)) {
-                texto.className = "match";
-            } else {
-                texto.className = "no-match";
-            }
-        } catch(e) {
-            texto.className = "";
+        const regex = new RegExp(expressao.value);
+        if(regex.test(texto.value)) {
+            texto.style.background = "#31f565"
+            status.textContent = "Válido";
+            status.style.color = "#31f565";
+        } else {
+            texto.style.background = "#f75145";
+            status.textContent = "Inválido";
+            status.style.color = "#f75145";
         }
     }
 
